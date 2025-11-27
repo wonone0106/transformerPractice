95009d4f43f028900b55865c4cc60035f31400ba
seungil <s24060@gsm.hs.kr>
2025-11-27 10:06:34 +0900

    class 정의

--- model/transformer.py
+++ model/transformer.py
@@ -12,3 +12,23 @@ class DecoderLayer(nn.Module):
 
 class MultiHeadAttention(nn.Module):
     pass
+
+class PositionalEncoding(nn.Module):
+    def __init__(self, input_dim, max_len=5000):
+        super().__init__()
+        pe = torch.zeros(max_len, input_dim)
+        position = torch.arange(0, max_len).unsqueeze(1).float()
+        div_term = torch.exp(
+            torch.arange(0, input_dim, 2).float()
+            * -(torch.log(torch.tensor(10000.0)) / input_dim)
+        )
+
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0)
+        self.register_buffer("pe", pe)
+
+    def forward(self, x):
+        pe = self.pe[:, : x.size(1)].to(x.device)
+        x = x + pe
+        return x
