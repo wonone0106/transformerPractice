4eb5473fc73facf02ea55cc0b2e60c9a26b6228c
seungil <s24060@gsm.hs.kr>
2025-12-05 11:53:54 +0900

    positionalEncoding 코드 수정

--- transformer.py
+++ transformer.py
@@ -114,12 +114,12 @@ class MultiHeadAttention(nn.Module):
 class PositionalEncoding(nn.Module):
     def __init__(self, embed_dim, max_len=5000):
         super().__init__()
-        pe = torch.zeros(max_len, embed_dim)
-        position = torch.arange(0, max_len).unsqueeze(1).float()
-        div_term = torch.arange(0, embed_dim, 2).float() ** (torch.tensor(10000.0) / embed_dim)
-        pe[:, 0::2] = torch.sin(position * div_term)
-        pe[:, 1::2] = torch.cos(position * div_term)
-        pe = pe.unsqueeze(0) 
+        pe = torch.zeros(max_len, embed_dim) # (max_len, embed_dim)
+        position = torch.arange(0, max_len).unsqueeze(1).float() # (max_len, 1)
+        div_term = torch.tensor(10000.0)  ** ( torch.arange(0, embed_dim, 2).float() / embed_dim) 
+        pe[:, 0::2] = torch.sin(position / div_term) 
+        pe[:, 1::2] = torch.cos(position / div_term)
+        pe = pe.unsqueeze(0) # (1, max_len, embed_dim)
         self.register_buffer('pe', pe)
 
     def forward(self, x):
