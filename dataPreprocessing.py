80dd5351361c441ee87b6e627818d4ca78f34b30
seungil <s24060@gsm.hs.kr>
2025-12-10 20:40:22 +0900

    tokenizer 변경 및 새로운 파일 추가

--- dataPreprocessing.py
+++ dataPreprocessing.py
@@ -1,10 +1,5 @@
 import torch
-from torch.utils.data import DataLoader
-from torchtext.data.utils import get_tokenizer
-from torchtext.vocab import build_vocab_from_iterator
-import spacy
-
-
+import sentencepiece as spm
 from glob import glob
 import pandas as pd
 
@@ -28,50 +23,28 @@ class Data:
         label = df['번역문']
         samples = [(src, tgt) for src, tgt in zip(txt, label)]
         return samples
-    
+
+sp = spm.SentencePieceProcessor()
+sp.load("tokenizer/spm.model")
 
 train_data = Data("train")
 valid_data = Data("valid")
 
-ko_tokenizer = get_tokenizer("spacy", language="ko_core_news_sm")
-en_tokenizer = get_tokenizer("spacy", language="en_core_web_sm")
-def ko_yield_tokens(data_iter):
-    for src, _ in data_iter:
-        token = ko_tokenizer(src)
-        yield token
-        
-def en_yield_tokens(data_iter):
-    for _, tgt in data_iter:
-        token = en_tokenizer(tgt)
-        yield token
-
-
-ko_vocab = build_vocab_from_iterator(ko_yield_tokens(train_data), specials=["<unk>", "<pad>", "<bos>", "<eos>"])
-ko_vocab.set_default_index(ko_vocab["<unk>"])
-
-en_vocab = build_vocab_from_iterator(en_yield_tokens(train_data), specials=["<unk>", "<pad>", "<bos>", "<eos>"])
-en_vocab.set_default_index(en_vocab["<unk>"])
-
 def collate_fn(batch):
     srcs, tgts, labels = [], [], []
+    bos_id = sp.bos_id()
+    eos_id = sp.eos_id()
+    pad_id = sp.pad_id()
     for src, tgt in batch:
-        srcs.append([ko_vocab[token] for token in ko_tokenizer(src)])
-        tgts.append([en_vocab["<bos>"]]+[en_vocab[token] for token in en_tokenizer(tgt)])
-        labels.append([en_vocab[token] for token in en_tokenizer(tgt)]+[en_vocab["<eos>"]])
-    max_length_question = max(len(text) for text in srcs)
-    max_length_answer = max(len(text) for text in tgts)
-    max_length_label = max(len(text) for text in labels)
-    padded_srcs = [text + [ko_vocab["<pad>"]] * (max_length_question - len(text)) for text in srcs]
-    padded_tgts = [text + [en_vocab["<pad>"]] * (max_length_answer - len(text)) for text in tgts]
-    padded_labels = [text + [en_vocab["<pad>"]] * (max_length_label - len(text)) for text in labels]
-    return {"src":torch.tensor(padded_srcs),
-            "tgt":torch.tensor(padded_tgts),
-            "label":torch.tensor(padded_labels)}
-    
-train_dl = DataLoader(train_data, batch_size=16, shuffle=True, collate_fn=collate_fn)
-valid_dl = DataLoader(valid_data, batch_size=16, shuffle=False, collate_fn=collate_fn)
-
-
-if __name__ == "__main__":
-    for src, tgt in train_dl:
-        breakpoint()
+        srcs.append(sp.encode(src))
+        tgts.append([bos_id] + sp.encode(tgt))
+        labels.append(sp.encode(tgt) + [eos_id])
+    max_src_len = max(len(s) for s in srcs)
+    max_tgt_len = max(len(t) for t in tgts)
+    for i in range(len(srcs)):
+        srcs[i] = srcs[i] + [pad_id] * (max_src_len - len(srcs[i]))
+        tgts[i] = tgts[i] + [pad_id] * (max_tgt_len - len(tgts[i]))
+        labels[i] = labels[i] + [pad_id] * (max_tgt_len - len(labels[i]))
+    return {"src": torch.tensor(srcs), 
+            "tgt": torch.tensor(tgts), 
+            "label": torch.tensor(labels)}
